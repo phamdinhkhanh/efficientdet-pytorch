678d4e305c7cbc29d1bf74694749a9dbc5c8a62b
phamdinhkhanh <phamdinhkhanh.tkt53.neu@gmail.com>
2021-03-30 16:31:18 +0700

    update instances

--- train.sh
+++ train.sh
@@ -1,1 +1,1 @@
-./distributed_train.sh 1 data --model tf_efficientdet_d4 --dataset coco -b 2 --local_rank 0 --amp --lr .09 --warmup-epochs 5 --sync-bn --opt momentum --model-ema
+./distributed_train.sh 1 ../../../kaggle/input --model tf_efficientdet_d4 --dataset coco -b 2 --local_rank 0 --amp --lr .09 --warmup-epochs 5 --sync-bn --opt momentum --model-ema
